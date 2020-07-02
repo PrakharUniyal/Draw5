f898b0bf1cd9f6d51ebdbe68fc2246c7ff416215
khyati18 <agarwal.khyati5@gmail.com>
2020-07-02 05:45:41 +0530

    abc

--- service/app.js
+++ service/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {spawn} = require('child_process');
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -18,7 +19,24 @@ db.once('open', function (callback) {
   console.log("connection succeeded!");
 });
 
-
+// app.get('/', (req, res) => {
+ 
+//  var dataToSend;
+//  // spawn new child process to call the python script
+//  const python = spawn('python', ['script1.py']);
+//  // collect data from script
+//  python.stdout.on('data', function (data) {
+//   console.log('Pipe data from python script ...');
+//   dataToSend = data.toString();
+//  });
+//  // in close event we are sure that stream from child process is closed
+//  python.on('close', (code) => {
+//  console.log(`child process close all stdio with code ${code}`);
+//  // send data to browser
+//  res.send(dataToSend)
+//  });
+ 
+// })
 
 app.get('/',function(req,res) {
     res.sendFile('index.html',{root:'./views'});
@@ -35,4 +53,4 @@ app.get('/memberHome',function(req,res) {
 require('./routes/users/signup')(app,db);
 require('./routes/members/signup')(app,db);
 
-app.listen(3000);
+app.listen(3000, () => console.log(`App listening on port 3000!`));
---
+++ service/script.py
@@ -0,0 +1,2 @@
+print("HELLO FROM PYTHON")
+
